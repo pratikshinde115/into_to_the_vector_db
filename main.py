6af57f501b020ff053a0c46875cb29958bb2dc0d
pratikshinde115 <pratikshinde115@gmail.com>
2024-01-12 15:26:33 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+import os
+from langchain.vectorstores import Pinecone
+import pinecone
+from langchain.chains import RetrievalQA
+
+
+from langchain_openai import OpenAI
+
+
+pinecone.init(api_key = "67be5599-aae8-4910-9a96-4f083e24679a" , environment = "gcp-starter")
+ 
+if __name__ == "__main__":
+        loaders = TextLoader('/home/pratik/Desktop/intro_to_vector_db/mediumblogs/mediumblog1.txt')
+
+
+        doc = loaders.load()
+        print(doc)
+        text_splitter = CharacterTextSplitter(chunk_size = 1000 , chunk_overlap = 0)
+        texts = text_splitter.split_documents(doc)
+        print(len(texts))
+
+        Embeddings = OpenAIEmbeddings(openai_api_key = os.environ.get("OPENAI_API_KEY"))
+        docsearch = Pinecone.from_documents(texts , Embeddings , index_name = 'medium-blog-embeddings-index')
+
+
+        qa = RetrievalQA.from_chain_type(
+                llm=OpenAI() , chain_type = "stuff" , retriever = docsearch.as_retriever()
+        )
+        querry = "what is similarity measures"
+        result = qa({'query' : querry})
+        print(result)
